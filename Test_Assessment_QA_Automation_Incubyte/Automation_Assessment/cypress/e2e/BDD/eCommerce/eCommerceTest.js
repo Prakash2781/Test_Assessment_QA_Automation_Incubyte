d48179e3d0a7907290e78d96fc1da20501fed0df
Prakash2781 <kashsk02781@gmail.com>
2024-12-22 17:14:10 +0530

    including L1 interview changes

--- Test_Assessment_QA_Automation_Incubyte/Automation_Assessment/cypress/e2e/BDD/eCommerce/eCommerceTest.js
+++ Test_Assessment_QA_Automation_Incubyte/Automation_Assessment/cypress/e2e/BDD/eCommerce/eCommerceTest.js
@@ -1,7 +1,7 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { fa, faker } from '@faker-js/faker';
 import Register_Page from "../../e_Commerce_Page/registration_Page";
-<<<<<<< HEAD
+
 import LoginPage from "../../e_Commerce_Page/LoginPage";
 
 const register = new Register_Page;
@@ -9,10 +9,6 @@ const login_Page = new LoginPage()
 
 let email = faker.internet.email()
 let password = "Test1234@"
-=======
-
-const register = new Register_Page;
->>>>>>> 3e5bf1cb105330326b4422e6c9d29864a82d9b31
 
 Given("The user is on the registration page", ()=>{
     register.home_page()
@@ -20,11 +16,9 @@ Given("The user is on the registration page", ()=>{
 })
 
 When("The user enters their name, email, and password", ()=>{
-<<<<<<< HEAD
+
     register.create_account(faker.person.firstName(), faker.person.lastName(), email, password, "Test1234@")
-=======
-    register.create_account(faker.person.firstName(), faker.person.lastName(), faker.internet.email(), "Test1234@", "Test1234@")
->>>>>>> 3e5bf1cb105330326b4422e6c9d29864a82d9b31
+
 })
 
 When("I submit the registration form", ()=>{
@@ -38,7 +32,6 @@ Then("The user should be redirected to the welcome page", ()=>{
     register.Validate_Login_Page_url()
 })
 
-<<<<<<< HEAD
 
 Given("The user is on the Login page", ()=>{
     login_Page.loginpage()
@@ -49,8 +42,3 @@ When("The user the email_Id and password", ()=>{
 Then("Validate successfully Logged-in Message", ()=>{
     login_Page.validate_url()
 })
-
-
-
-=======
->>>>>>> 3e5bf1cb105330326b4422e6c9d29864a82d9b31
